0b92a70ee896eb2f7f14035c4b67e7ac64277e4a
wrathematics <wrathematics@gmail.com>
2016-03-09 15:40:39 -0500

    fix spacing weirdness

--- bootstrap.sh
+++ bootstrap.sh
@@ -10,11 +10,11 @@ export PBDHOME=/home/pbdRchive # FIXME?
 
 ### Directory structure
 # pbdRchive
-# 				 ├── bin
-# 				 ├── bootstrap
-# 				 ├── dev
-#					 ├── docs
-# 				 └── website
+#          ├── bin
+#          ├── bootstrap
+#          ├── dev
+#          ├── docs
+#          └── website
 
 ### Paths and shit, whatevs it's cool
 cd ${PBDHOME}
@@ -24,7 +24,7 @@ export MAKE="/usr/bin/make -j $(( $NCORES+1 ))"
 CODENAME=`lsb_release -c | sed -e "s/Codename:\t//"`
 
 if [ ! -a ~/.inputrc ]; then 
-	echo "\$include /etc/inputrc" > ~/.inputrc
+  echo "\$include /etc/inputrc" > ~/.inputrc
 fi
 echo "set completion-ignore-case On" >> ~/.inputrc
 
@@ -42,11 +42,11 @@ sudo add-apt-repository ppa:chris-lea/libsodium -y
 sudo apt-get -qq update 
 sudo apt-get -qq upgrade
 sudo apt-get install vim git tmux \ 
-	build-essential libclang-dev clang gfortran \
-	libcurl4-openssl-dev libzmq3-dev libsodium-dev \
-	openmpi-bin libopenmpi-dev libnetcdf-dev \
-	libopenblas-dev libatlas-dev aspell libaspell-dev
-	
+  build-essential libclang-dev clang gfortran \
+  libcurl4-openssl-dev libzmq3-dev libsodium-dev \
+  openmpi-bin libopenmpi-dev libnetcdf-dev \
+  libopenblas-dev libatlas-dev aspell libaspell-dev
+  
 
 ### Set up R
 sudo cat "deb http://cran.rstudio.com/bin/linux/ubuntu $CODENAME/" >> /etc/apt/sources.list
@@ -62,12 +62,12 @@ names=`curl -s https://api.github.com/users/${user}/repos | grep full_name | sed
 cd ${PBDHOME}/dev
 
 for repo in ${names};do
-	cd ${PBDHOME}/dev
-	# if [ -d "$repo" ]; then
-	# 	cd $x && git pull
-	# else
-		git clone "https://github.com/$repo" "$repo"
-	# fi
+  cd ${PBDHOME}/dev
+  # if [ -d "$repo" ]; then
+  #   cd $x && git pull
+  # else
+    git clone "https://github.com/$repo" "$repo"
+  # fi
 done
 
 cd ${PBDHOME}/dev
